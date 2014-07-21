ffcb8a17ac14bcaba76d25f3ef2eb400e7bdeb80
Paul Liétar <paul@lietar.net>
2014-07-21 15:36:30 +0200

    arm-none-eabi-gcc: Update newlib to 2.1.0

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -8,8 +8,8 @@ class ArmNoneEabiGcc < Formula
 
   # http://sourceware.org/newlib/
   resource 'newlib' do
-    url 'ftp://sourceware.org/pub/newlib/newlib-2.0.0.tar.gz'
-    sha1 'ea6b5727162453284791869e905f39fb8fab8d3f'
+    url 'ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz'
+    sha1 '364d569771866bf55cdbd1f8c4a6fa5c9cf2ef6c'
   end
 
   depends_on 'gmp'
@@ -24,7 +24,11 @@ class ArmNoneEabiGcc < Formula
 
   def install
     resource('newlib').stage do
-        (buildpath).install Dir['newlib', 'libgloss']
+      # Reported and fixed upstream
+      # http://comments.gmane.org/gmane.comp.lib.newlib/8879
+      Patch.create(:p1, :DATA).apply
+
+      (buildpath).install Dir['newlib', 'libgloss']
     end
 
     # See https://gcc.gnu.org/ml/gcc/2014-05/msg00014.html
@@ -79,3 +83,31 @@ class ArmNoneEabiGcc < Formula
   end
 end
 
+__END__
+diff --git a/libgloss/arm/configure b/libgloss/arm/configure
+index cc7e570..bdd4b13 100644
+--- a/libgloss/arm/configure
++++ b/libgloss/arm/configure
+@@ -2551,7 +2551,7 @@ esac
+
+
+
+-host_makefile_frag=${srcdir}/../config/default.mh
++host_makefile_frag=`cd $srcdir/../config;pwd`/default.mh
+
+ host_makefile_frag_path=$host_makefile_frag
+
+diff --git a/libgloss/arm/configure.in b/libgloss/arm/configure.in
+index d617f49..f9409ca 100644
+--- a/libgloss/arm/configure.in
++++ b/libgloss/arm/configure.in
+@@ -59,7 +59,8 @@ esac
+
+ AC_SUBST(objtype)
+
+-host_makefile_frag=${srcdir}/../config/default.mh
++host_makefile_frag=`cd $srcdir/../config;pwd`/default.mh
++
+
+ dnl We have to assign the same value to other variables because autoconf
+ dnl doesn't provide a mechanism to substitute a replacement keyword with
